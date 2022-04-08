6ad72db8a65def1283856f9577d336ac412ad484
Mirage <andrej_malinin@mail.ru>
2022-04-08 18:46:05 +0300

    Lexer small fixes

--- Compiler/include/Lexer.h
+++ Compiler/include/Lexer.h
@@ -18,15 +18,15 @@ enum LexemesTypes: const int{
 };
 
 class Lexer : public yyFlexLexer{
+public:
+    int yylex() override;
+    void GetLexemes(std::vector<std::pair<int, std::string>>* lexemes, const std::string& path);
+
+private:
     int lexeme_type_;
     std::string value_;
-
     int Process(int type);
     int ProcessError();
-
-  public:
-    int yylex() override;
-    void GetLexemes(std::vector<std::pair<int, std::string>> *lexemes, const std::string& path);
 };
 
 #endif //COMPILER_LEXER_H
